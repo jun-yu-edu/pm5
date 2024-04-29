debf0f2de4345936a31b2f7acbbaacdf3858d5f2
jun <junyu.edu@gmail.com>
2024-04-29 16:37:48 +0900

    a

---
+++ 0429/vue_prac/App_prac_5.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <input v-model="newText">
+      <input type="submit" value="제출">
+    </form>
+    <ul>
+      <li v-for="todo in todos">
+        {{ todo.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+
+let newText = ref('')
+
+const todos = reactive([
+  { text: 'Hello' },
+  { text: 'World' },
+  { text: 'and Vue!' }
+])
+function onSubmit(){
+  // console.log(newText.value)
+  todos.push( { text : newText.value })
+  newText = ref('')
+}
+
+</script>
+
+<style lang="scss" scoped>
+</style>
