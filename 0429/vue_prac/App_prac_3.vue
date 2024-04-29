3e0b726ab2465179b3c1c519fa95f55549ea3306
jun <junyu.edu@gmail.com>
2024-04-29 15:41:49 +0900

    a

---
+++ 0429/vue_prac/App_prac_3.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="url in urls">
+      <!-- {{ url }} -->
+      <a :href="url.url">{{ url.name }} </a>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const urls = [
+  { name: 'naver', url: 'https://www.naver.com/' },
+  { name: 'google', url: 'https://www.google.com/' },
+  { name: 'github', url: 'https://github.com/'},
+  { name: 'vue', url: 'https://ko.vuejs.org/'},
+]
+</script>
+
+<style lang="scss" scoped>
+
+</style>
