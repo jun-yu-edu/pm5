6fec489e22c0da34ea92a7e35b1d5f1de95ea414
jun <junyu.edu@gmail.com>
2024-04-30 17:23:01 +0900

    a

---
+++ 0430/router_prac/src/views/MyView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>This is My View</h1>
+
+    <button @click="goHome">홈으로</button>
+    <button @click="goAbout">어바웃으로</button>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+function goHome() {
+  router.push('/')
+}
+
+function goAbout() {
+  router.push('/about')
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 0430/router_prac/src/views/NumberView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>
+      {{ num }}
+    </h1>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+const num = route.params.num
+
+</script>
