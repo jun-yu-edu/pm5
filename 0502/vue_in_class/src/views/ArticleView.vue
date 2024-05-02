8cb67599f943bec87ed4b59aad905d0196b1e011
jun <junyu.edu@gmail.com>
2024-05-02 11:34:54 +0900

    a

--- 0502/vue_in_class/src/views/ArticleView.vue
+++ 0502/vue_in_class/src/views/ArticleView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="articleStore.fetchArticles">article 가져오기!</button>
+    <!-- <button @click="articleStore.fetchArticles">article 가져오기!</button> -->
     <!-- {{ articleStore.articles }} -->
     <ul>
       <li v-for="article in articleStore.articles">
@@ -14,8 +14,12 @@
 
 <script setup>
 import { useArticleStore } from '../stores/articles';
+import { onMounted } from 'vue';
 
 const articleStore = useArticleStore()
+onMounted(() => {
+  articleStore.fetchArticles()
+})
 </script>
 
 <style lang="scss" scoped>
